cd271368e1f2e590695faab980a1f34630837adb
Marian_O <oromerom@guanajuato.gob.mx>
2024-04-18 14:22:37 -0600

    add module

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basic_operations',
+    version='0.1',
+    packages=find_packages(),
+    author='Mariano O.R',
+    author_email='marianolivo.romero@gmail.com',
+    description='Paquete que proporciona funciones para hacer suma, resta y multiplicación de un número',
+    url='https://github.com/DeveGnu/basic_operations.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
